77b8d5f995d8e3ab37dc4e8ce801c7da8a97a7fa
ianfr93 <albitox10@gmail.com>
2024-02-16 01:11:02 -0300

    usar

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -7,31 +7,33 @@ let usuarios = [
   { usuario: "usuario6", contraseña: "contraseña6" }
 ];
 
-let nombreUsuario;
 let intentosRestantes = 4;
 
 function autenticarUsuario() {
-  nombreUsuario = prompt("Ingrese su nombre de usuario");
-  let contraseña = prompt("Ingrese su contraseña");
+  let nombreUsuario = document.getElementById("username").value;
+  let contraseña = document.getElementById("password").value;
+
   let usuarioValido = usuarios.find(user => user.usuario === nombreUsuario && user.contraseña === contraseña);
 
   if (!usuarioValido) {
     alert(`Nombre de usuario o contraseña incorrectos. Intentos restantes: ${intentosRestantes}`);
     intentosRestantes--;
 
-    if (intentosRestantes > 0) {
-      autenticarUsuario();
-    } else {
+    if (intentosRestantes <= 0) {
       alert("¡Se han agotado los intentos! Reinicie la sesión para intentar nuevamente.");
       intentosRestantes = 4;
+      resetForm();
     }
   } else {
     alert(`¡Bienvenido, ${nombreUsuario}! Has iniciado sesión exitosamente.`);
-    // Aquí podrías redirigir al usuario a otra página o realizar otras acciones necesarias después del inicio de sesión exitoso
+    resetForm();
+    // Puedes redirigir al usuario a otra página o realizar otras acciones necesarias después del inicio de sesión exitoso
   }
 }
 
-// autenticarUsuario();
+function resetForm() {
+  document.getElementById("loginForm").reset();
+}
 
 
 //VARIABLES
