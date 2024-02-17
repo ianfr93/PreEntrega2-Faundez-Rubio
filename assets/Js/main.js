47536747ee23eee720693259c4a489f07fcb687a
ianfr93 <albitox10@gmail.com>
2024-02-17 01:03:06 -0300

    cambios

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,16 +1,33 @@
-let usuarios = [
-  { usuario: "usuario1", contraseña: "contraseña1" },
-  { usuario: "usuario2", contraseña: "contraseña2" },
-  { usuario: "usuario3", contraseña: "contraseña3" },
-  { usuario: "usuario4", contraseña: "contraseña4" },
-  { usuario: "usuario5", contraseña: "contraseña5" },
-  { usuario: "usuario6", contraseña: "contraseña6" }
+let usuarios = [{
+    usuario: "usuario1",
+    contraseña: "contraseña1"
+  },
+  {
+    usuario: "usuario2",
+    contraseña: "contraseña2"
+  },
+  {
+    usuario: "usuario3",
+    contraseña: "contraseña3"
+  },
+  {
+    usuario: "usuario4",
+    contraseña: "contraseña4"
+  },
+  {
+    usuario: "usuario5",
+    contraseña: "contraseña5"
+  },
+  {
+    usuario: "usuario6",
+    contraseña: "contraseña6"
+  }
 ];
 
 let intentosRestantes = 4;
 
 function autenticarUsuario(event) {
-  event.preventDefault();  // Prevenir el comportamiento predeterminado del formulario
+  event.preventDefault(); // Prevenir el comportamiento predeterminado del formulario
 
   let nombreUsuario = document.getElementById("username").value;
   let contraseña = document.getElementById("password").value;
@@ -26,7 +43,7 @@ function autenticarUsuario(event) {
 
     if (intentosRestantes > 0) {
       mensajeElement.innerHTML = `Nombre de usuario o contraseña incorrectos. Intentos restantes: ${intentosRestantes}`;
-      mensajeElement.className = "alert alert-danger";
+      mensajeElement.className = "error-message";
       mensajeElement.style.display = "block";
     } else {
       mensajeElement.innerHTML = "¡Se han agotado los intentos! Reinicie la sesión para intentar nuevamente.";
@@ -38,6 +55,8 @@ function autenticarUsuario(event) {
   }
 }
 
-document.getElementById("acceso").addEventListener("click", autenticarUsuario);
-
-
+document.getElementById("acceso").addEventListener("click", function (event) {
+  if (intentosRestantes > 4) {
+    autenticarUsuario(event);
+  }
+});
