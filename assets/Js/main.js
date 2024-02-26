f886b857f7874c848bd52040d9d1151949ec9a2b
ianfr93 <albitox10@gmail.com>
2024-02-26 02:02:44 -0300

    fu

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -159,17 +159,20 @@ function verInventario() {
   console.log("Inventario actualizado:");
   productos.forEach(producto => console.log(producto));
 }
-
+// Función para mostrar el menú principal
 function mostrarMenu() {
   let seguirEnSistema = true;
 
   while (seguirEnSistema) {
     let opcion = prompt('Selecciona una opción:\n1. Ir a mi perfil de usuario\n2. Ir al home\n3. Ver inventario\n4. Salir');
 
-    if (opcion === null || opcion === '') {
-      // Si el usuario hace clic en Cancelar o ingresa una cadena vacía, tratamos como si hubiera seleccionado salir
+    if (opcion === null) {
+      // Si el usuario hace clic en Cancelar, tratamos como si hubiera seleccionado salir
       alert('Saliendo del sistema. ¡Hasta luego!');
       seguirEnSistema = false;
+    } else if (opcion === '') {
+      // Si el usuario ingresa una cadena vacía, le informamos que la opción es incorrecta y continuamos el bucle
+      alert('Opción incorrecta. Digite nuevamente para continuar en el sistema');
     } else {
       switch (opcion) {
         case '1':
